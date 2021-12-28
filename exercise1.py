4e463b1dd2566c69fdd2a1a01dcb3a7f54c18113
Luca Masserano <l.masserano@yahoo.it>
2021-12-28 16:16:22 +0100

    Update exercise1.py
    Exercise 1: Listing pineapple routes

--- exercise1.py
+++ exercise1.py
@@ -1,19 +1,22 @@
+import math
+import random
+from io import StringIO
 portnames = ["PAN", "AMS", "CAS", "NYC", "HEL"]
  
 def permutations(route, ports):
     # write the recursive function here
     # remember to print out the route as the recursion ends
-	a = len(ports)
-	if a > 1:
-		for i in ports:
-			route.append(i)
-			ports_a=ports.copy()
-			ports_a.remove(i)
-			permutations(route, ports_a)
-			route.remove(i)
-	else:
-		route = route + ports
-		print(' '.join([portnames[i] for i in route]))
+    a = len(ports)
+    if a > 1:
+        for i in ports:
+            route.append(i)
+            ports_a=ports.copy()
+            ports_a.remove(i)
+            permutations(route, ports_a)
+            route.remove(i)
+    else:
+        route = route + ports
+        print(' '.join([portnames[i] for i in route]))
 
 # this will start the recursion with 0 as the first stop
 permutations([0], list(range(1, len(portnames))))
