68dea0047c995165f9b9e457d3e209a5dc3432a8
Luca Masserano <l.masserano@yahoo.it>
2021-12-28 16:41:10 +0100

    Create exercise11.py
    Exercise 11: Real estate price predictions

---
+++ exercise11.py
@@ -0,0 +1,19 @@
+import math
+import random
+import numpy as np
+from io import StringIO
+# input values for three mökkis: size, size of sauna, distance to water, number of indoor bathrooms, 
+# proximity of neighbors
+X = [[66, 5, 15, 2, 500], 
+     [21, 3, 50, 1, 100], 
+     [120, 15, 5, 2, 1200]]
+c = [3000, 200, -50, 5000, 100]    # coefficient values
+
+def predict(X, c):
+    for i in X:
+        price = 0
+        for j in range(len(i)):
+            price = price + c[j]*i[j]            
+        print(price)
+
+predict(X, c)
